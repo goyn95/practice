b907f66456f5be94f954ecbb890cb6070a36a979
고영남 <goyn9501@gmail.com>
2021-09-04 16:39:07 +0900

    git project

---
+++ src/practice1.java
@@ -0,0 +1,17 @@
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		
+		int result = 0;
+		for (int i = 0; i < 10; i++) {
+			for(int j = 0; j<10; j++) {
+				result = (i + 1)*(j+1);
+				System.out.println(result);
+			}
+		}
+		
+
+	}
+
+}
